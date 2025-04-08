527178228022d541cbf4df69c6877192287d65af
Rinkesh Sante <rinkeshsante@gmail.com>
2025-04-08 14:05:56 +0530

    failling one

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -18,7 +18,7 @@ test("adds 1.5 + 2.5 to equal 4", () => {
 });
 
 test("adds -1 + 1 to equal 0", () => {
-  expect(sum(-1, 0)).toBe(0);
+  expect(sum(-1, 1)).toBe(0);
 });
 
 test("adds large numbers 1000000 + 2000000 to equal 3000000", () => {
